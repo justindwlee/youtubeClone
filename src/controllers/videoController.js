69bdbe46a8dba1a16e6878a9b9b6850366a381a4
Justin Lee <dwlee1399@gmail.com>
2025-02-06 17:02:39 +0900

    Lecture ~5.10

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,9 +1,35 @@
-export const trending = (req, res) => res.send("Trending Videos");
-export const see = (req, res) => {
-  console.log(req.params);
-  res.send(`Watch Video No.${req.params.id}`);
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "Video#1",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Video#2",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 2,
+    },
+    {
+      title: "Video#3",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 3,
+    },
+  ];
+  return res.render("home", { pageTitle: "Home", videos });
 };
-export const edit = (req, res) => res.send("Edit Video");
+export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+
+export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const search = (req, res) => res.send("Search videos here");
 export const upload = (req, res) => res.send("Upload Video");
 export const deleteVideo = (req, res) => res.send("Delete Video");
--- src/server.js
+++ src/server.js
@@ -7,8 +7,10 @@ import videoRouter from "./routers/videoRouter";
 const PORT = 4000;
 
 const app = express();
-app.use(morgan("tiny"));
 
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+app.use(morgan("tiny"));
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 app.use("/", globalRouter);
